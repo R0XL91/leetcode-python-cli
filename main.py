566153c5b65245589d58453c8720767b8d1fbc96
Raul <raul.leal.ii91@gmail.com>
2025-08-06 21:27:45 +0200

    Fixed minor bugs and added feature to save exercise in any path

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import argparse
-import subprocess
 
 from utils.api import fetch_problem
-from utils.utils import create_problem_structure
+from utils.utils import create_problem_structure, run_problem_tests
 
 
 def main():
@@ -10,23 +9,27 @@ def main():
         description="CLI to download LeetCode problems, generate structure, or run tests."
     )
     parser.add_argument(
-        'action', choices=['fetch', 'test'], help='Action to perform: fetch or test'
+        'action', choices=['fetch', 'test'],
+        help='Action to perform: fetch or test'
     )
     parser.add_argument(
-        'name', type=str, help='Problem slug (for fetch) or test name (for test)'
+        'name', type=str,
+        help='Problem slug (for fetch) or test name (for test)'
+    )
+    parser.add_argument(
+        '--path', type=str, default='problems',
+        help='Path to the problems directory (default: problems)'
     )
     args = parser.parse_args()
 
     if args.action == 'fetch':
         try:
-            problem = fetch_problem(args.name)
-            create_problem_structure(problem)
+            problem = fetch_problem(slug=args.name)
+            create_problem_structure(problem=problem, path=args.path)
         except (RuntimeError, ValueError) as e:
             print(f"Error: {e}")
     elif args.action == 'test':
-        # Run pytest for the specified test
-        test_path = f"problems/tests/{args.name}/test.py"
-        subprocess.run(['pytest', '-s', test_path], check=False)
+        run_problem_tests(slug=args.name, path=args.path)
 
 
 if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,17 @@
 import importlib.util
 import os
+import subprocess
 import sys
+from typing import List
 
 from jinja2 import Environment, FileSystemLoader
 
 from prototypes import Problem
 
 
-def create_src_dir(problem: Problem, env: Environment):
+def create_src_dir(problem: Problem, env: Environment, path: str = "problems"):
     """Create the src directory for a LeetCode problem."""
-    base_dir = os.path.join(os.getcwd(), f"problems/src/{problem.slug}")
+    base_dir = get_path([path, "src", problem.slug])
     os.makedirs(base_dir, exist_ok=True)
     # Create README.md and solution.py using Jinja2 templates
     readme_template = env.get_template("README.md.j2")
@@ -20,35 +22,47 @@ def create_src_dir(problem: Problem, env: Environment):
         f.write(solution_template.render(problem=problem))
 
 
-def create_test_dir(problem: Problem, env: Environment):
+def create_test_dir(problem: Problem, env: Environment, path: str = "problems"):
     """Create the test directory for a LeetCode problem."""
-    test_dir = os.path.join(os.getcwd(), f"problems/tests/{problem.slug}")
+    test_dir = get_path([path, "tests", problem.slug])
     os.makedirs(test_dir, exist_ok=True)
     # Create test file using Jinja2 template
     test_template = env.get_template("test.py.j2")
+    project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     with open(os.path.join(test_dir, "test.py"), "w", encoding="utf-8") as f:
-        f.write(test_template.render(problem=problem))
+        f.write(test_template.render(problem=problem, path=path, project_path=project_path))
 
 
-def create_problem_structure(problem: Problem):
+def create_problem_structure(problem: Problem, path: str = "problems"):
     """Create the folder and files for a LeetCode problem using Jinja2 templates."""
     # Create src and test directory if it doesn't exist
     environment = Environment(
         loader=FileSystemLoader(os.path.join(os.getcwd(), "templates")),
         autoescape=False,
     )
-    create_src_dir(problem, environment)
-    create_test_dir(problem, environment)
+    create_src_dir(problem, environment, path)
+    create_test_dir(problem, environment, path)
 
 
-def import_problem_module(slug):
+def import_problem_module(slug: str, path: str = "problems"):
+    """Import the problem module dynamically based on the slug."""
     module_name = slug
-    module_path = f"{os.path.sep}".join(
-        [os.getcwd(), "problems", "src", module_name, f"{module_name}.py"]
-    )
+    module_path = get_path([path, "src", module_name, f"{module_name}.py"])
     spec = importlib.util.spec_from_file_location(module_name, module_path)
     problem_module = importlib.util.module_from_spec(spec)
     sys.modules[module_name] = problem_module
     spec.loader.exec_module(problem_module)
-
     return problem_module
+
+
+def run_problem_tests(slug: str, path: str = "problems"):
+    path = get_path([path, "tests", slug, "test.py"])
+    subprocess.run(['pytest', '-s', path], check=False)
+
+
+def get_path(path: List[str]) -> str:
+    """Get the absolute path for a given relative path."""
+    path = f"{os.path.sep}".join(path)
+    if not os.path.isabs(path):
+        return os.path.join(os.getcwd(), path)
+    return path
